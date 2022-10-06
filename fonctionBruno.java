efc2f03d4abe66f3a8e7dc9e13d35c84e6493d87
BrunoDefalque <defalquebruno@gmail.com>
2022-10-06 15:20:10 +0200

    function String et Int

--- fonctionBruno.java
+++ fonctionBruno.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
+
 /**
  * fonctionBruno
  */
 public class fonctionBruno {
 
-	public static void Name() {System.out.println("David");
+	public static Scanner userInput = new Scanner(System.in);
+
+	public static String UserName() {
+
+		System.out.println(" entrer votre prénom");
+
+		String _firstName = userInput.nextLine();
+
+		return _firstName;
+}
+
+public static int NombreJoueur() {
+
+	System.out.println(" combien y aura-t-il de joueurs ? Minimum 1 joueur et maximum 4");
+
+	int _number = userInput.nextInt();
+
+	if (_number <1 ) {
+
+		System.out.println("Vous devez jouer au moin à un joueur !!");
+	}
+
+	if (_number > 4) {
+
+		System.out.println("Le nombre de joueur maximum est de 4");
+	}
+
+	return _number;
 }
 }
