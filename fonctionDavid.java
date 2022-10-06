bc58d791d9744378725c123d3f1db4f56ea3024a
David Dehoust <dehoustdavid@gmail.com>
2022-10-06 15:13:38 +0200

    ajout du dossier .vscode et début du fichier quiz.java

---
+++ fonctionDavid.java
@@ -0,0 +1,13 @@
+public class fonctionDavid {
+
+    public static String[] creerTableauString(String[] _TableauDeBase, int _taille){
+
+        String[] _tableau = new String [_TableauDeBase.length + 3];
+
+        for (int i = 0; i < _taille; i++) {
+            _tableau[i] = "sans nom";
+        }
+
+        return _tableau;
+    }
+}
--- quiz.java
+++ quiz.java
@@ -1,12 +1,33 @@
 public class quiz {
     public static void main(String[] args) {
 
+        // Les variables
+
+        int nombreJoueurs = 3;
+
         String[] nomJoueurs = {};
-        
+
+        boolean partieEnCour = false;
+
+
+
+
         System.out.println("Bienvenu dans notre quiz");
 
-        System.out.println("combien de joueur ?");
+        System.out.println("combien de joueur êtes-vous?");
 
-        // demande du nombre de joueur
+        nomJoueurs = fonctionDavid.creerTableauString(nomJoueurs, 3);
+
+        System.out.println(nomJoueurs.length);
+
+        // fonction qui demande un nombre
+
+        if (nombreJoueurs > 1) {
+            // lance le mode solo
+        } else {
+            // lance le mode multi
+        }
+
+        System.out.println("Entrez votre nom");
     }
 }
