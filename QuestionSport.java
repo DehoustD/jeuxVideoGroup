5e1d5f067304e09f4836bc2d87b0906b7b3d6639
David Dehoust <dehoustdavid@gmail.com>
2022-10-11 10:04:44 +0200

    vide du fichier sport

--- QuestionSport.java
+++ QuestionSport.java
@@ -1,16 +1,4 @@
 public class QuestionSport {
 
-    String sportQ1 = "combien de ballons au foot ?";
 
-    String[] sportR1 = {"1","2","3","4"};
-
-    String sportQ2 = "combien de raquettes au tennis ?";
-
-    String[] sportR2 = {"1","2","3","4"};
-
-    // tableau des questions et des réponses
-
-    String[] tableauQuestionSport = {sportQ1, sportQ2};
-
-    String[][] tableauReponseSport = {sportR1, sportR2};
 }
